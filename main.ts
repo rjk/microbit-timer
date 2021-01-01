fdf388470dd51ec999382558e15c5dab3b8e1ffc
Rory <rory.kingan@gmail.com>
2021-01-02 03:25:35 +1300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     alarmMins = alarmMins + 1
     basic.showNumber(alarmMins)
 })
-function ShowTimer (num: number) {
+function ShowTimer (num: number, animate: boolean) {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -13,7 +13,9 @@ function ShowTimer (num: number) {
     x = 0
     y = 0
     for (let index = 0; index <= num - 1; index++) {
-        basic.pause(100)
+        if (animate) {
+            basic.pause(100)
+        }
         led.plot(x, y)
         x = x + 1
         if (x == 5) {
@@ -27,19 +29,24 @@ input.onButtonPressed(Button.AB, function () {
     basic.showNumber(alarmMins)
 })
 input.onButtonPressed(Button.B, function () {
-    ShowTimer(alarmMins)
+    ShowTimer(alarmMins, true)
     startTimeMs = input.runningTime()
-    endTimeMs = startTimeMs + alarmMins * 1000
-    minsLeft = alarmMins
+    endTimeMs = startTimeMs + alarmMins * msPerMinute
+    minsLeft = Math.floor((endTimeMs - input.runningTime()) / msPerMinute)
     while (input.runningTime() < endTimeMs) {
-        while ((endTimeMs - input.runningTime()) / 1000 < minsLeft) {
-            FlashLed(minsLeft)
-            basic.pause(200)
+        console.log("start: " + startTimeMs + ", end: " + endTimeMs 
+            + ", minsLeft: " + minsLeft + ", runtime: " + input.runningTime())
+        ShowTimer(minsLeft+1, false)
+        while (input.runningTime() < endTimeMs - minsLeft * msPerMinute) {
+            console.log(" - start: " + startTimeMs + ", end: " + endTimeMs 
+                + ", minsLeft: " + minsLeft + ", runtime: " + input.runningTime())
+            FlashLed(minsLeft+1)
+            basic.pause(100)
         }
-        minsLeft = (endTimeMs - input.runningTime()) / 1000
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        minsLeft = Math.floor((endTimeMs - input.runningTime()) / msPerMinute)
     }
-    ShowTimer(minsLeft)
-    music.playTone(262, music.beat(BeatFraction.Double))
+    soundExpression.giggle.play()
     basic.showIcon(IconNames.Heart)
 })
 function FlashLed (ledNo: number) {
@@ -51,10 +58,12 @@ function FlashLed (ledNo: number) {
     led.toggle(x, y)
 }
 let led2 = 0
-let minsLeft = 0
-let endTimeMs = 0
-let startTimeMs = 0
 let y = 0
 let x = 0
+let msPerMinute = 0
 let alarmMins = 0
+let startTimeMs = 0
+let endTimeMs = 0
+let minsLeft = 0
 alarmMins = 0
+msPerMinute = 5000
